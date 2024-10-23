ff3e2922fe7ea7ed77e29f32719086b3773e8882
AdrienPIZZO <adrien_04100@hotmail.fr>
2024-10-23 14:50:57 +0200

    adding bootstrap through npm and adding ngx-bootstrap

--- src/app/project-modal/project-modal.component.ts
+++ src/app/project-modal/project-modal.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../_models/Project';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-project-modal',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './project-modal.component.html',
   styleUrl: './project-modal.component.scss'
 })
